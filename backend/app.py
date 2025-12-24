a608409695e817523340f834bc08b16b80c53d4b
GALACTIC-PRETTY-BOY <160834862+GALACTIC-PRETTY-BOY@users.noreply.github.com>
2025-12-24 12:28:51 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -102,3 +102,10 @@ def stop():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+@app.route("/test")
+def test():
+    if API_KEY:
+        return "API key loaded ✅"
+    else:
+        return "API key NOT found ❌"
